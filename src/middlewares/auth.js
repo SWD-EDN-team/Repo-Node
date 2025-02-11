4e37cc2b0d538579cbc77d89e1088f5db8c75050
huynguyen30042003 <nguyenkhoanhathuy@gmail.com>
2025-02-11 15:35:57 +0700

    update router user

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -32,3 +32,29 @@ export const admin = async (req, res, next) => {
   }
 };
 
+
+export const user = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1]; // Lấy token từ header
+
+  if (!token) {
+    return res.status(StatusCode.UNAUTHORIZED).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Xác thực token
+    
+    const user = await  User.findById(decoded.id);
+    console.log("user",user);
+    if (user.refreshToken === undefined) {
+      return res.status(StatusCode.UNAUTHORIZED).json({
+        message: "Token has expired, please login again",
+      });
+    }
+    
+    req.user = decoded; // Gắn thông tin user vào req để sử dụng sau
+    next();
+  } catch (err) {
+    return res.status(StatusCode.FORBIDDEN).json({ message: "Invalid token" });
+  }
+};
+
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,15 +1,25 @@
-import express from 'express';
-import { signup,signin,getAll,getByEmail,getCurrentUser,refreshToken,logout } from '../controllers/auth.js';
-import {admin} from '../middlewares/auth.js';
+import express from "express";
+import {
+  signup,
+  signin,
+  getAll,
+  getByEmail,
+  getCurrentUser,
+  refreshToken,
+  logout,
+  updateUser,
+} from "../controllers/auth.js";
+import { admin, user } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/auth/signup",signup)
-router.post("/auth/signin",signin)
-router.get("/auth/getall",admin,getAll)
-router.get("/auth/getByEmail",admin,getByEmail)
-router.get("/auth/getCurrentUser",getCurrentUser)
-router.post("/auth/refreshToken",refreshToken)
-router.post("/auth/logout",logout)
+router.post("/auth/signup", signup);
+router.post("/auth/signin", signin);
+router.get("/auth/getall", admin, getAll);
+router.get("/auth/getByEmail", admin, getByEmail);
+router.get("/auth/getCurrentUser", getCurrentUser);
+router.post("/auth/refreshToken", refreshToken);
+router.post("/auth/logout", logout);
+router.put("/auth/update/:email", user, updateUser);
 
 export default router;
