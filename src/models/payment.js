a5f875da1a56313db222ae4da734a01570931101
Đào Bá Anh Quân <quandba154@gmail.com>
2025-03-08 15:11:52 +0700

    Merge branch 'develop' into quandba-develop

--- src/models/payment.js
+++ src/models/payment.js
@@ -2,31 +2,31 @@ import mongoose, { Schema } from "mongoose";
 
 const paymentSchema = new Schema(
   {
-    customer_id: {
+    customer_id:{
       type: String,
       required: true,
       unique: true,
     },
-    payment_date: {
+    payment_date:{
       type: Date,
       required: true,
       default: Date.now(),
     },
-    amount: {
+    amount:{
       type: Number,
       required: true,
     },
-    status_payment: {
+    status_payment:{
       type: String,
       required: true,
       default: "pending",
       enum: ["pending", "paid", "rejected"],
     },
-    payment_method: {
+    payment_method:{
       type: Schema.Types.ObjectId,
-      ref: "PaymentMethod",
+      ref: 'PaymentMethod',
       required: true,
-    },
+    }
   },
   { timestamps: false, versionKey: false }
 );
